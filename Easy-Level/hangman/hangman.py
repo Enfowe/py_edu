b5c2c399c18dea664eee17f90ff39583330e3af7
enfys11 <opptreda@gmail.com>
2021-02-01 20:45:00 +0300

    Палач Stage 3

--- Easy-Level/hangman/hangman.py
+++ Easy-Level/hangman/hangman.py
@@ -2,7 +2,8 @@ import random
 print("H A N G M A N")
 words = ['python', 'java', 'kotlin', 'javascript']
 correct_word = random.choice(words)
-player_word = input("Guess the word:")
+hidden_word = correct_word[:3] + "-" * (len(correct_word) - 3)
+player_word = input(f"Guess the word: {hidden_word}")
 if player_word == correct_word:
     print("You survived!")
 else:
