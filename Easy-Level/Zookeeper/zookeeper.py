28f84892e6da4b5f6697fcd93a0adec8a92db6cb
enfys11 <opptreda@gmail.com>
2021-02-02 22:43:00 +0300

    Зоопарк (Верблюд)

--- Easy-Level/Zookeeper/zookeeper.py
+++ Easy-Level/Zookeeper/zookeeper.py
@@ -1,6 +1,20 @@
-print("""I love animals!
-Let's check on the animals...
-The deer looks fine.
-The bat looks happy.
-The lion looks healthy.
-""")
+print(r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!""")
