d6db126e4e625c2244d180379c785590895132c4
enfys11 <opptreda@gmail.com>
2021-02-02 23:02:44 +0300

    Крестики-нолики

--- Easy-Level/Tic-Tac-Toe/tictactoe.py
+++ Easy-Level/Tic-Tac-Toe/tictactoe.py
@@ -1,36 +1,67 @@
-start_pos = input("Enter cells: ")
-cells = [[start_pos[0], start_pos[1], start_pos[2]],
-         [start_pos[3], start_pos[4], start_pos[5]],
-         [start_pos[6], start_pos[7], start_pos[8]]]
+# JetBrains Academy
+# Project: Tic Tac Toe
+# Stage 5/5
 
-с = """[[0, 1, 2],
-         [3, 4, 5],
-         [6, 7, 8],
-         [0, 3, 6],
-         [1, 4, 7],
-         [2, 5, 8],
-         [0, 4, 8],
-         [2, 4, 6]]"""
-        
-moves = {(1, 3): cells[0][0], (2, 3): cells[0][1], (3, 3): cells[0][2],
-         (1, 2): cells[1][0], (2, 2): cells[1][1], (3, 2): cells[1][2],
-         (1, 1): cells[2][0], (2, 1): cells[2][1], (3, 1): cells[2][2]}
+moves = {(1, 3): "_", (2, 3): "_", (3, 3): "_",
+         (1, 2): "_", (2, 2): "_", (3, 2): "_",
+         (1, 1): "_", (2, 1): "_", (3, 1): "_"}
+
+
+def update_combs():
+    global combs
+    combs = [[moves[1, 3], moves[2, 3], moves[3, 3]],
+             [moves[1, 2], moves[2, 2], moves[3, 2]],
+             [moves[1, 1], moves[2, 1], moves[3, 1]],
+             [moves[1, 3], moves[1, 2], moves[1, 1]],
+             [moves[2, 3], moves[2, 2], moves[2, 1]],
+             [moves[3, 3], moves[3, 2], moves[3, 1]],
+             [moves[1, 3], moves[2, 2], moves[3, 1]],
+             [moves[1, 1], moves[2, 2], moves[3, 3]]]
 
 
 def print_field():
     print(f"""---------
-| {moves[(1,3)]} {moves[(2,3)]} {moves[(3,3)]} |
-| {moves[(1,2)]} {moves[(2,2)]} {moves[(3,2)]} |
-| {moves[(1,1)]} {moves[(2,1)]} {moves[(3,1)]} |
+| {moves[(1, 3)]} {moves[(2, 3)]} {moves[(3, 3)]} |
+| {moves[(1, 2)]} {moves[(2, 2)]} {moves[(3, 2)]} |
+| {moves[(1, 1)]} {moves[(2, 1)]} {moves[(3, 1)]} |
 ---------""")
 
+
 print_field()
+
+
+def states():
+    o_win = None
+    x_win = None
+    
+    if "_" in moves.values():
+        empty = True
+    else:
+        empty = False
+
+    for comb in combs:
+        first, second, third = comb
+        if first == second == third:
+            if first == "X":
+                x_win = True
+            if first == "O":
+                o_win = True
+
+    impossible = o_win and x_win
+    draw = not o_win and not x_win and not empty
+    game_over = not empty
+    return (x_win, o_win, impossible, draw, game_over)
+
+
+current_char = "X"
+prev_char = "O"
 input_ = True
 while input_:
     try:
+        print("Enter the coordinates: ")
         move = tuple([int(num) for num in input().split()])
     except:
-        print("You should enter numbers!") 
+        print("You should enter numbers!")
         continue
     if move not in moves:
         print("Coordinates should be from 1 to 3!")
@@ -38,6 +69,25 @@ while input_:
     if moves[move] != "_":
         print("This cell is occupied! Choose another one!")
         continue
-    moves[move] = "X"
-    input_ = False
-print_field()
+    moves[move] = current_char
+    current_char, prev_char = prev_char, current_char
+
+    print_field()
+
+    update_combs()
+    x_win, o_win, impossible, draw, game_over = states()
+
+    if impossible:
+        print("Impossible")
+        input_ = False
+    elif draw:
+        print("Draw")
+        input_ = False
+    elif x_win:
+        print("X wins")
+        input_ = False
+    elif o_win:
+        print("O wins")
+        input_ = False
+    else:
+        continue
