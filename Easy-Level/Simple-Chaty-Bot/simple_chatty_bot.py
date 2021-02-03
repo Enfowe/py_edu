8befeb2e682b507600e1df36f2dfafb2d8a3dfec
enfys11 <opptreda@gmail.com>
2021-02-03 21:27:12 +0300

    Консольный чат бот

--- Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
+++ Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
@@ -1,24 +1,62 @@
-print('Hello! My name is Mailo.')
-print('I was created in 2021.')
-print('Please, remind me your name.')
+# JetBrains Academy
+# Project: Simple Chaty Bot
+# Stage 5/5
 
-name = input()
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-num = int(input())
-for num in range(num + 1):
-    print(f"{num} !")
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-print('Completed, have a nice day!')
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("""Let's test your programming knowledge.
+Why do we use methods?
+1. To repeat a statement multiple times.
+2. To decompose a program into several small subroutines.
+3. To determine the execution time of a program.
+4. To interrupt the execution of a program.""")
+    answer = int(input())
+    while answer != 2:
+        print("Please, try again.")
+        answer = int(input())
+
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
