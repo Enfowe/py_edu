e71bcc5f4951d4350e72e7c110d9e05721ba7289
enfys11 <opptreda@gmail.com>
2021-02-03 21:03:00 +0300

    Консольный чат бот 1

--- Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
+++ Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
@@ -1,2 +1,7 @@
-print("""Hello! My name is Mailo.
-I was created in 2021.""")
+print('Hello! My name is Mailo.')
+print('I was created in 2021.')
+print('Please, remind me your name.')
+
+your_name = input()
+
+print(f'What a great name you have, {your_name}!')
