d3109f3058bd44eb3e5906a0c322315bef059e5f
enfys11 <opptreda@gmail.com>
2021-02-03 21:05:00 +0300

    Консольный чат бот 2

--- Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
+++ Easy-Level/Simple-Chaty-Bot/simple_chatty_bot.py
@@ -2,6 +2,13 @@ print('Hello! My name is Mailo.')
 print('I was created in 2021.')
 print('Please, remind me your name.')
 
-your_name = input()
+name = input()
 
-print(f'What a great name you have, {your_name}!')
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+remainder3, remainder5, remainder7 = int(input()), int(input()), int(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
