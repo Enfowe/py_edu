07f4489832f3929c801d5807d674528e2e75e988
enfys11 <opptreda@gmail.com>
2021-02-04 20:31:15 +0300

    Coffee-Machine fix

--- Coffee-Machine/coffee_machine.py
+++ Coffee-Machine/coffee_machine.py
@@ -1,88 +1,77 @@
-money = 550
-water = 400
-milk = 540
-coffee_beans = 120
-disposable_cups = 9
-working = True
+class Machine:
 
+    def __init__(self):
+        self.money = 550
+        self.water = 400
+        self.milk = 540
+        self.coffee_beans = 120
+        self.disposable_cups = 9
+        self.working = True
+        # tuple decoding in dict "drinks":
+        # (drink name, ml of water, ml of milk, g of coffee beans, cost in $)
+        self.drinks = {"1": ("espresso", 250, 0, 16, 4),
+                       "2": ("latte", 350, 75, 20, 7),
+                       "3": ("cappuccino", 200, 100, 12, 6)}
+        self.actions = {"buy": self.buy,
+                        "fill": self.fill,
+                        "take": self.take,
+                        "remaining": self.remaining,
+                        "exit": self.exit}
 
-def remaining():
-    print()
-    print(f"""The coffe machine has:
-{water} ml of water
-{milk} ml of milk
-{coffee_beans} g of coffee beans
-{disposable_cups} of disposable cups
-{money} of money
-""")
+    def remaining(self):
+        print()
+        print(f"""The coffee machine has:\n{self.water} ml of water\n{self.milk} ml of milk
+{self.coffee_beans} g of coffee beans\n{self.disposable_cups} of disposable cups\n{self.money} of money""")
 
-
-def buy():
-    global money
-    global water
-    global milk
-    global coffee_beans
-    global disposable_cups
-    # tuple decoding in dict "drinks":
-    # (drink name, ml of water, ml of milk, g of coffee beans, cost in $)
-    drinks = {"1": ("espresso", 250, 0, 16, 4),
-              "2": ("latte", 350, 75, 20, 7),
-              "3": ("cappuccino", 200, 100, 12, 6)}
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-    choice = input()
-    if choice == "back":
-        return None
-    drink = drinks[choice]
-    if water >= drink[1]:
-        if milk >= drink[2]:
-            if coffee_beans >= drink[3]:
-                if disposable_cups >= 1:
-                    print("I have enough resources, making you a coffee!")
-                    water -= drink[1]
-                    milk -= drink[2]
-                    coffee_beans -= drink[3]
-                    money += drink[4]
-                    disposable_cups -= 1
+    def buy(self):
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+        choice = input()
+        if choice == "back":
+            return None
+        drink = self.drinks[choice]
+        if self.water >= drink[1]:
+            if self.milk >= drink[2]:
+                if self.coffee_beans >= drink[3]:
+                    if self.disposable_cups >= 1:
+                        print("I have enough resources, making you a coffee!")
+                        self.water -= drink[1]
+                        self.milk -= drink[2]
+                        self.coffee_beans -= drink[3]
+                        self.money += drink[4]
+                        self.disposable_cups -= 1
+                    else:
+                        print("Sorry, no disposable cups")
                 else:
-                    print("Sorry, no disposable cups")
+                    print("Sorry, not enough coffee beans")
             else:
-                print("Sorry, not enough coffee beans")
+                print("Sorry, not enough milk")
         else:
-            print("Sorry, not enough milk")
-    else:
-        print("Sorry, not enough water")
+            print("Sorry, not enough water")
+
+    def fill(self):
+        print("Write how many ml of water do you want to add:")
+        self.water += int(input())
+        print("Write how many ml of milk do you want to add:")
+        self.milk += int(input())
+        print("Write how many grams of coffee beans do you want to add:")
+        self.coffee_beans += int(input())
+        print("Write how many disposable cups of coffee do you want to add:")
+        self.disposable_cups += int(input())
+
+    def take(self):
+        print(f"I gave you ${self.money}")
+        self.money = 0
+
+    def exit(self):
+        self.working = False
+
+    def work(self):
+        while self.working:
+            print("Write action (buy, fill, take, remaining, exit):")
+            action = input()
+            self.actions[action]()
+            print()
 
 
-def fill():
-    global water
-    global milk
-    global coffee_beans
-    global disposable_cups
-    print("Write how many ml of water do you want to add:")
-    water += int(input())
-    print("Write how many ml of milk do you want to add:")
-    milk += int(input())
-    print("Write how many grams of coffee beans do you want to add:")
-    coffee_beans += int(input())
-    print("Write how many disposable cups of coffee do you want to add:")
-    disposable_cups += int(input())
-
-
-def take():
-    global money
-    print(f"I gave you ${money}")
-    money = 0
-
-
-def exit_():
-    global working
-    working = False
-
-
-actions = {"buy": buy, "fill": fill, "take": take, "remaining": remaining, "exit": exit_}
-
-while working:
-    print("Write action (buy, fill, take, remaining, exit):")
-    action = input()
-    actions[action]()
-    print()
+coffee_machine = Machine()
+coffee_machine.work()
