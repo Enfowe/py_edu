65db3eed6748d33c2ad036ff4fc9ac7732986488
enfys11 <opptreda@gmail.com>
2021-02-04 20:01:13 +0300

    Coffee-Machine 2

--- Coffee-Machine/coffee_machine.py
+++ Coffee-Machine/coffee_machine.py
@@ -1,6 +1,15 @@
+print("Write how many ml of water the coffee machine has:")
+water = int(input("> "))
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input("> "))
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee = int(input("> "))
 print("Write how many cups of coffee you will need:")
-cups = int(input("> "))
-print(f"For {cups} cups of coffee you will need:")
-print(f"{200 * cups} ml of water")
-print(f"{50 * cups} ml of milk")
-print(f"{15 * cups} g of coffee beans")
+need_cups = int(input("> "))
+can_make = min([water // 200, milk // 50, coffee // 15])
+if can_make < need_cups:
+    print(f"No, I can make only {can_make} cups of coffee")
+elif can_make == need_cups:
+    print("Yes, I can make that amount of coffee")
+else:
+    print(f"Yes, I can make that amount of coffee (and even {can_make - need_cups} more than that)")
