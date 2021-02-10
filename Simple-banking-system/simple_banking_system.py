a751d1cc40d399f5aa8ad98b757b388706f6b336
enfys11 <opptreda@gmail.com>
2021-02-10 20:15:45 +0300

    simple_banking_system sqlite

--- Simple-banking-system/simple_banking_system.py
+++ Simple-banking-system/simple_banking_system.py
@@ -1,16 +1,18 @@
+# JetBrains Academy
+# Project: Simple Banking System
+# Stage 4/4
+
 import random
 import sqlite3
 
 conn = sqlite3.connect('card.s3db')
 cur = conn.cursor()
-if open("card.s3db", "r").readlines() == []:
-    cur.execute('CREATE TABLE card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
-    conn.commit()
+cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
+conn.commit()
 
 
 class Bank:
     def __init__(self):
-        self.database = {}
         self.current_user = None
         self.is_run = True
 
@@ -25,7 +27,8 @@ class Bank:
         else:
             return
 
-    def luhn_checksum(self, card_number):
+    @staticmethod
+    def luhn_checksum(card_number):
         numbers = list(map(int, list(card_number)))
         for i in range(len(numbers)):
             if i % 2 == 0:
@@ -36,10 +39,9 @@ class Bank:
         id_num = str(random.randint(1, 999999999)).rjust(9, "0")
         card_number = "400000" + id_num
         card_number += str(self.luhn_checksum(card_number))
-
-        if card_number not in self.database:
+        in_db = cur.execute(f"SELECT number FROM card WHERE number = '{card_number}'").fetchall()
+        if not in_db:
             card_pin = str(random.randint(1, 9999)).rjust(4, "0")
-            self.database[card_number] = card_pin
             print(f"Your card has been created\nYour card number:\n{card_number}\nYour card PIN:\n{card_pin}")
             cur.execute(f'INSERT INTO card (id, number, pin) VALUES ({id_num}, "{card_number}", "{card_pin}")')
             conn.commit()
@@ -47,18 +49,68 @@ class Bank:
         else:
             return self.create_an_account()
 
+    def add_income(self):
+        income = int(input("Enter income:"))
+        cur.execute(f"SELECT balance FROM card WHERE number = '{self.current_user}'")
+        balance = cur.fetchone()[0]
+        balance += income
+        cur.execute(f"UPDATE card SET balance = {balance} WHERE number = '{self.current_user}'")
+        conn.commit()
+        print("Income was added!")
+        return self.account_page()
+
+    def do_transfer(self):
+        receiver_card = input("Transfer\nEnter card number:")
+        luhn_checksum = self.luhn_checksum(receiver_card[0: -1])
+        all_cards = [card[0] for card in cur.execute("SELECT number FROM card;").fetchall()]
+        if str(luhn_checksum) != receiver_card[-1]:
+            print("Probably you made a mistake in the card number. Please try again!")
+        elif receiver_card not in all_cards:
+            print("Such a card does not exist.")
+        elif receiver_card == self.current_user:
+            print("You can't transfer money to the same account!")
+        else:
+            money_to_transfer = int(input("Enter how much money you want to transfer:"))
+            user_balance = cur.execute(f"SELECT balance FROM card WHERE number = '{self.current_user}';").fetchone()[0]
+            receiver_balance = cur.execute(f"SELECT balance FROM card WHERE number = '{receiver_card}';").fetchone()[0]
+            if money_to_transfer > user_balance:
+                print("Not enough money!")
+            else:
+                user_balance -= money_to_transfer
+                receiver_balance += money_to_transfer
+                cur.execute(f"UPDATE card SET balance = {user_balance} WHERE number = '{self.current_user}';")
+                cur.execute(f"UPDATE card SET balance = {receiver_balance} WHERE number = '{receiver_card}';")
+                conn.commit()
+                print("Success!")
+        return self.account_page()
+
+    def close_account(self):
+        cur.execute(f"DELETE FROM card WHERE number = '{self.current_user}'")
+        conn.commit()
+        print("The account has been closed!")
+        return self.start_page()
+
     def account_page(self):
         commands = {"1": self.check_balance,
-                    "2": self.log_out,
+                    "2": self.add_income,
+                    "3": self.do_transfer,
+                    "4": self.close_account,
+                    "5": self.log_out,
                     "0": self.exit}
-        print("1. Balance\n2. Log out\n0. Exit")
+        print("1. Balance\n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit")
         command = input()
         return commands.get(command, self.account_page)()
 
     def log_into_account(self):
         card_number = input("Enter your card number:")
-        card_pin = input("Enter your PIN:")
-        if self.database.get(card_number) == card_pin:
+        user_pin = input("Enter your PIN:")
+        in_db = cur.execute(f"SELECT number FROM card WHERE number = '{card_number}'").fetchall()
+        if in_db:
+            card_pin = cur.execute(f"SELECT pin FROM card WHERE number = '{card_number}'").fetchone()[0]
+        else:
+            print("Wrong card number or PIN!")
+            return self.start_page()
+        if card_pin == user_pin:
             print("You have successfully logged in!")
             self.current_user = card_number
             return self.account_page()
@@ -67,8 +119,8 @@ class Bank:
             return self.start_page()
 
     def check_balance(self):
-        balance = self.database.get(self.current_user)
-        print("Balance: 0")
+        balance = cur.execute(f"SELECT balance FROM card WHERE number = '{self.current_user}'").fetchone()[0]
+        print(f"Balance: {balance}")
         return self.account_page()
 
     def log_out(self):
